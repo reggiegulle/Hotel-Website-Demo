69826e0c0290c63979211a48355d8a67a98a12c1
Reggie Gulle <reggie.gulle@gmail.com>
2016-02-12 19:22:17 +1100

    Now included src and dest file tree
    From this point onward, the commits will contain the source and
    destination folders and files.

---
+++ src/js/hotel.js
@@ -0,0 +1,245 @@
+/*
+Javascript for Hotels Web Template
+*/
+
+$(document).ready(function(){
+	
+	//clear the searchbox
+	$("#customSearch").val("");
+	
+	//set the style of the
+	//owlhotel li img
+	/*$("#owlhotel li img").css({
+		"height":($("#owlhotel li img").width() * 0.5625) + "px"
+	});
+	
+	$(window).resize(function(){
+		$("#owlhotel li img").css({
+			"height":($("#owlhotel li img").width() * 0.5625) + "px"
+		});
+	});*/
+
+	
+	
+	//initialize DataTables with capital "D"
+	var hoteltable = $('#hoteltable').dataTable({
+		'ajax':'hotel.json',
+		'dom': '<"row"<"col-sm-6 col-xs-12"i>>rt<"row"<"col-sm-6 col-xs-12"i>>',
+		'order': [[0, 'asc']],
+		'infoCallback': function( settings, start, end, max, total, pre ) {
+				return start + ' to ' + end + ' of ' + max + ' items';
+			},
+		'columnDefs': [
+                {
+                    'data':'index',
+                    'className': 'never',
+                    'targets': 0  
+                },
+                {
+                    'data':'name', 
+                    'className': 'all', 
+                    'targets': 1
+                },
+                {
+                    'data': 'image',
+                    'render': function(data, type, row, meta){
+                        return '<div class="table-thumb"><img src="images/' + data.src + '-small.jpg" width="100%" height="100%" alt="' + data.src + '" data-video_id="' + data.video_id + '"></div>';
+                    }, 
+                    'className': 'all', 
+                    'orderable': false, 
+                    'targets': 2
+                },
+				{
+                    'data': 'price',
+                    'render': {
+                        '_': 'price_display',
+                        'filter': 'price_filter',
+                        'sort': 'price_sort',
+                        'display': 'price_display'
+                    },
+                    'className': 'min-tablet', 
+                    'type': 'num-fmt', 
+                    'targets': 3
+                },
+                {
+                    'data': 'category',
+                    'render': {
+                        '_': 'category_display',
+                        'filter': 'category_filter',
+                        'sort': 'category_sort',
+                        'display': 'category_display'
+                    },
+                    'className': 'min-tablet', 
+                    'targets': 4
+                },
+                {
+                    'data': 'description', 
+                    'className': 'none', 
+                    'orderable': false, 
+                    'targets': 5
+                },
+                {
+                    'data': 'control',
+                    'className': 'all control', 
+                    'orderable': false, 
+                    'targets': -1
+                }
+			],
+		'autoWidth': true,
+		'scrollY': '600',
+		'paging': false,
+		'info': true,
+		'responsive': {
+                    details:{
+                        'type': 'column',
+                        'target': -1
+                    }
+                }
+	});
+    
+    $('input.form-control.input-sm').val('');
+    
+    
+    
+    //explicitly set the sort and filter select options
+    //for Mozilla Firefox
+    $("#sort-name option:eq(0)").prop('selected', true);
+    $("#sort-price option:eq(0)").prop('selected', true);
+    $("#category-filter option:eq(0)").prop('selected', true);
+
+	function populateOwlCarousels(){
+		var hoteltable = $('#hoteltable').DataTable();
+        
+        //remove items from upper carousel
+        //right after each rendering of the table
+        while($('#owl-hotel-1 li').length > 0){
+			owl_hotel_1.removeItem(0);
+		}
+        
+        //remove items from bottom carousel
+        //right after each rendering of the table
+        while($('#owl-hotel-2 li').length > 0){
+			owl_hotel_2.data('owlCarousel').removeItem(0);
+		}
+        
+        var rowData;
+        
+        $('#hoteltable tbody tr').each(function(){
+                var thisRow = $(this);
+                //get the data (json data source) from each row
+                rowData = hoteltable.row(thisRow).data();
+                
+                //add the relevant data to the owl-carousel bottom list
+                //one list item at a time
+                if (typeof(rowData) != 'undefined'){
+                    var owl_hotel_1_li_item = '<li>';
+                        owl_hotel_1_li_item += '<div class="owl-hotel-1-slide" data-video_id="' + rowData.image.video_id + '" data-imgsrc="' + rowData.image.src + '">';
+                            owl_hotel_1_li_item += '<div class="owl-hotel-1-slide-overlay-player" data-video_id="' + rowData.image.video_id + '">';
+                                owl_hotel_1_li_item += '<button class="play-button">';
+                                    owl_hotel_1_li_item += 'Play Video';
+                                owl_hotel_1_li_item += '</button>';
+                            owl_hotel_1_li_item += '</div>';
+                        owl_hotel_1_li_item += '</div>';
+                    owl_hotel_1_li_item += '</li>';
+                    owl_hotel_1.addItem(owl_hotel_1_li_item);
+                }
+                
+                //add the relevant data to the owl-carousel bottom list
+                //one list item at a time
+                if (typeof(rowData) != 'undefined'){
+                    var owl_hotel_2_li_item = '<li>';
+                    owl_hotel_2_li_item += '<div class="owl-carousel-2-slide">';
+                    owl_hotel_2_li_item +='<img src="images/' + rowData.image.src + '-small.jpg" width="100%" height="100%" alt="' + rowData.image.src + '" data-video_id="' + rowData.image.video_id + '"></div>';
+                    owl_hotel_2_li_item += '<h6>';
+                    owl_hotel_2_li_item += rowData.name;
+                    owl_hotel_2_li_item += '</h6>';
+                    owl_hotel_2_li_item += '<p>(';
+                    owl_hotel_2_li_item += rowData.category.category_filter;
+                    owl_hotel_2_li_item += ')</p>';
+                    owl_hotel_2_li_item += '</li>';
+                    owl_hotel_2.data('owlCarousel').addItem(owl_hotel_2_li_item);
+                }
+        });
+	}
+	
+    var hoteltable = $('#hoteltable').DataTable();
+    
+   
+    
+	$("#sort-name").change(function(){
+        $("#sort-price option:eq(0)").prop('selected', true);
+        var sort_name_value = $(this).val();
+		switch (sort_name_value){
+            case 'reset':
+                hoteltable.order([0, "asc"]).draw();
+            break;
+            case 'asc':
+                hoteltable.order([1, "asc"]).draw(); 
+            break;
+            case 'desc':
+                hoteltable.order([1, "desc"]).draw();
+            break;
+            default:
+            break;
+        }     
+	});
+    
+    $(".sort-name-reset").click(function(){
+        $("#sort-name option:eq(0)").prop('selected', true);
+        $("#sort-price option:eq(0)").prop('selected', true);
+        hoteltable.order([0, "asc"]).draw(); 
+    });
+    
+    $("#sort-price").change(function(){
+        $("#sort-name option:eq(0)").prop('selected', true);
+        var sort_name_value = $(this).val();
+		switch (sort_name_value){
+            case 'reset':
+                hoteltable.order([0, "asc"]).draw();
+            break;
+            case 'lowtohigh':
+                hoteltable.order([3, "asc"]).draw(); 
+            break;
+            case 'hightolow':
+                hoteltable.order([3, "desc"]).draw();
+            break;
+            default:
+            break;
+        }     
+	});
+    
+    $('.hoteltable-custom-search input').on('keyup', function(){
+        hoteltable.search(this.value).draw();
+    });
+    
+    $(".sort-price-reset").click(function(){
+        $("#sort-price option:eq(0)").prop('selected', true);
+        $("#sort-name option:eq(0)").prop('selected', true);
+        hoteltable.order([0, "asc"]).draw(); 
+    });
+    
+    $("#category-filter").change(function(){
+        var custom_filter_value = $(this).val();
+        if(custom_filter_value == 'reset'){
+            hoteltable.columns().search('').draw();
+        } else {
+            hoteltable.column(4).search(custom_filter_value).draw();
+        }    
+	});
+    
+    $('.category-filter-reset').click(function(){
+        $("#category-filter option:eq(0)").prop('selected', true);
+        hoteltable.columns().search('').draw();
+    });
+    
+   
+    $('.all-reset').click(function(){
+        $('input.form-control.input-sm').val('');
+        hoteltable.search('').draw();
+        $("#category-filter option:eq(0)").prop('selected', true);
+        hoteltable.columns().search('').draw();
+        $("#sort-name option:eq(0)").prop('selected', true);
+        $("#sort-price option:eq(0)").prop('selected', true);
+        hoteltable.order([0, "asc"]).draw();
+    });
+});
---
+++ src/js/hotel_youtube.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+    
+    var hoteltable = $('#hoteltable').DataTable();
+    
+    hoteltable.on('draw.dt', function(){
+        
+        /*
+        assign a "true" bool value
+        to the "owl_hotel_1_playing" variable
+        corresponding to the "autoPlay:on" of owl-hotel-1
+        
+        var owl_hotel_1_playing = true;
+        
+        //behaviour for recognising status of overlay player
+        if ($('#owl-hotel-1 li').length != 0){
+            
+            
+            the block below is a special block
+            for stopping the first slide from autoplaying
+            the FIRST TIME that the owl-hotel-1 initializes
+            if the "Play Video" button is pressed
+            
+            $('#owl-hotel-1 li div.owl-hotel-1-slide-overlay-player:eq(0) button').click(function(){
+                var video_id = $(this).parent('div').data('video_id');
+                //check value of var owl_hotel_1_playing bool
+                if(owl_hotel_1_playing === true){
+                    
+                    if value is true,
+                    change it to "false"
+                    
+                    owl_hotel_1_playing = false;
+                    //then stop owl-hotel-1 autoplay
+                    $('#owl-hotel-1').trigger('owl.stop');
+                }
+                
+                replace the "Play Video" button
+                with an iframe YouTube embedded player
+                
+                $(this).parent('div').addClass('player-activated').html('<iframe id="ytplayer" type="text/html" width="100%" height="100%" src="http://www.youtube.com/embed/' + video_id + '?autoplay=1&rel=0&controls=2&rel=0 frameborder="0"/>');
+            }); 
+            
+            
+            clicking on the specified items below
+            will restore the owl-hotel-1 autoplay feature 
+            
+            $('#owl-hotel-1.owl-theme .owl-controls .owl-buttons div, #owl-hotel-2 li').click(function(){
+                //check value of var owl_hotel_1_playing bool
+                if (owl_hotel_1_playing === false){
+                    
+                    if value is false,
+                    change it to "true"
+                    
+                    owl_hotel_1_playing = true;
+                    //then resume owl-hotel-1 autoplay
+                    $('#owl-hotel-1').trigger('owl.play', 6000);
+                }
+            }); 
+            
+            a custom event 'allowPlayerActivation'
+            enables changing the content
+            of each owl-hotel-1-overlay dynamically
+            ----referenced in hotel.js 'function sycnPosition'
+            
+            $('#owl-hotel-1').on('allowPlayerActivation', function(){
+                $('#owl-hotel-1 li div.owl-hotel-1-slide-overlay-player').each(function(){
+                    if($(this).hasClass('player-activated')){
+                        $(this).removeClass('player-activated');
+                        
+                        replace the iframe YouTube embed
+                        with the default content of "Play Video" button
+                        
+                        $(this).html('<button class="play-button">Play Video</button>');
+                    }
+                });
+                
+                $('#owl-hotel-1 li div.owl-hotel-1-slide-overlay-player button').each(function(){
+                    var video_id = $(this).parent('div').data('video_id');
+                    $(this).click(function(){
+                        ///check value of var owl_hotel_1_playing bool
+                        if(owl_hotel_1_playing = true){
+                            
+                            if value is true,
+                            change it to "false"
+                            
+                            owl_hotel_1_playing = false;
+                            //then stop owl-hotel-1 autoplay
+                            $('#owl-hotel-1').trigger('owl.stop');
+                        }
+                        
+                        replace the "Play Video" button
+                        with an iframe YouTube embedded player
+                        
+                        $(this).parent('div').addClass('player-activated').html('<iframe id="ytplayer" type="text/html" width="100%" height="100%" src="http://www.youtube.com/embed/' + video_id + '?autoplay=1&rel=0&controls=2&rel=0 frameborder="0"/>'); 
+                    });  
+                }); 
+            });
+        }
+        */
+    });
+    
+});
